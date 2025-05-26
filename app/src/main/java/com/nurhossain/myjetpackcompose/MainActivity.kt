9b1ee4f5e82d12f7473cd860034b9b30b32f5eb4
HossainNur <nurhossainsani1@gmail.com>
2025-05-26 18:14:45 +0600

    button add

--- app/src/main/java/com/nurhossain/myjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/nurhossain/myjetpackcompose/MainActivity.kt
@@ -2,27 +2,40 @@ package com.nurhossain.myjetpackcompose
 
 import android.os.Bundle
 import android.os.Message
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,6 +56,7 @@ class MainActivity : ComponentActivity() {
                     //UserProfile(name ="Nur Hossain" , age = 25 )
                     Subject(favSub = "english", generalSub = "math")
                 }
+                MyButton()
             }
         }
     }
@@ -117,6 +131,49 @@ fun UserBio(name: String,bio: String) {
     Column {
         Text(text = name, style = MaterialTheme.typography.headlineMedium)
         Text(text = bio, lineHeight = 24.sp)
+    }
+}
+
+@Composable
+fun MyButton() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        val context = LocalContext.current
+
+        Button(
+            onClick = {
+                Toast.makeText(context, "Welcome to Geeks for Geeks", Toast.LENGTH_LONG).show()
+            },
+            modifier = Modifier.padding(16.dp),
+            enabled = true,
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.Green,
+                containerColor = Color.Black
+            ),
+            elevation = ButtonDefaults.buttonElevation(defaultElevation = 10.dp),
+            border = BorderStroke(width = 2.dp, brush = SolidColor(Color.Green)),
+            contentPadding = PaddingValues(
+                start = 20.dp,
+                top = 12.dp,
+                end = 20.dp,
+                bottom = 12.dp
+            ),
+            interactionSource = remember { MutableInteractionSource() }
+        ) {
+            Text(
+                text = "Geeks for Geeks",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic,
+                fontFamily = FontFamily.Serif
+            )
+        }
     }
 }
 
